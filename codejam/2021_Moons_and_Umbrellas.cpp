561c5b8bb61c5f26e0ebf013912aea07c6246623
yuweichen1008 <yuweiichen@gmail.com>
2021-03-27 13:45:50 +0800

    MLE fix

--- codejam/2021_Moons_and_Umbrellas.cpp
+++ codejam/2021_Moons_and_Umbrellas.cpp
@@ -4,17 +4,6 @@
 
 using namespace std;
 
-void permutation(int index, string s, vector<string>& store)
-{
-    if(index == s.size()){
-        store.push_back(s);
-    }else if(s[index] == '?'){
-        permutation(index+1, s.substr(0,index) + 'C' + s.substr(index+1), store);
-        permutation(index+1, s.substr(0,index) + 'J' + s.substr(index+1), store);
-    }else{
-        permutation(index+1, s, store);
-    }
-};
 int countRepeat(string b, string a){
     int cnt = 0;
     for(int i = 0; i < a.size(); i++)
@@ -24,7 +13,20 @@ int countRepeat(string b, string a){
         }
     }
     return cnt;
-}
+};
+void permutation(int index, string s, int& minval, int& x, int& y)
+{
+    if(index == s.size()){
+        int cntx = countRepeat("CJ", s);
+        int cnty = countRepeat("JC", s);
+        minval = min(minval, cntx * x + cnty * y);
+    }else if(s[index] == '?'){
+        permutation(index+1, s.substr(0,index) + 'C' + s.substr(index+1), minval, x, y);
+        permutation(index+1, s.substr(0,index) + 'J' + s.substr(index+1), minval, x, y);
+    }else{
+        permutation(index+1, s, minval, x, y);
+    }
+};
 int main()
 {
     int t;
@@ -37,19 +39,7 @@ int main()
         vector<string> store;
         cin >> x >> y;
         cin >> s;
-
-        permutation(0, s, store);
-
-        for(int si = 0; si < store.size(); si++){
-            string s = store[si];
-            // cout << "string : " << s << endl;
-            int cntx, cnty;
-
-            cntx = countRepeat("CJ", s);
-            cnty = countRepeat("JC", s);
-
-            minval = min(minval, cntx * x + cnty * y);
-        }
+        permutation(0, s, minval, x, y);
         cout << "Case #" << ti+1 << ": " << minval << endl;
     }
     return 0;
